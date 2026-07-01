da97831ba06663e04e8c38f2218f8e2c2fa55c98
Shivansh Pardeshi <shivanshpardeshi90@gmail.com>
2026-07-01 20:59:53 +0530

    Add files via upload

---
+++ 4_operators.py
@@ -0,0 +1,33 @@
+# Arithmetic operators 
+
+a=55
+b=99
+print(a+b) 
+
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+a= 55-33 
+print(a)  # assign 55-33 in a
+
+b+= 5 # increment the value of b by 5 and them assign it to b 
+print(b)
+
+
+# comparsion operators 
+
+c=10>66
+print(c)
+
+d =10<=10
+print(d)
+
+e = 10==10
+print(e)
+
+# logical operators 
+
+f= True or False
+print(f)
